a9732a064d7cd440f52a28edfa3040de14dcf42b
Patrick PREMARTIN <patrick@premartin.fr>
2022-08-12 12:37:13 +0200

    aded FCKEditor config sample file

---
+++ src/fckconfig.js
@@ -0,0 +1,4 @@
+FCKConfig.Plugins.Add( 'acronym', 'en,fr' ) ;
+FCKConfig.Plugins.Add( 'language', 'en,fr' ) ;
+FCKConfig.Plugins.Add( 'citation', 'en,fr' ) ;
+FCKConfig.Plugins.Add( 'abbreviation', 'en,fr' ) ;
